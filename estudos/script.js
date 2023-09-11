bddf2a62d9c48982e69592c9e9c5fbcd3ed37a2c
giovanimohrboso <giovani.mohr@gmail.com>
2023-09-11 17:00:17 -0300

    teste

---
+++ estudos/script.js
@@ -0,0 +1,11 @@
+function abrirModal() {
+    const modal = document.getElementById('janela_modal')
+
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click',(e) =>{
+        if (e.target.id == 'fechar' || e.target.id == 'janela_modal'){
+            modal.classList.remove('abrir')
+        }
+    })
+}
